09e337fa75754433c1a6d336cc58869d13162b8e
Chris MITEL <mitel.chris@gmail.com>
2018-12-14 02:30:52 +0100

    Cleans the  default messages config

--- src/assets/config/messages.config.ts
+++ src/assets/config/messages.config.ts
@@ -6,28 +6,11 @@ const messages: any[] = [
   {
     msg: `Hello guys! My name is Chris and I'm a french software engineer.`,
     date: new Date(),
-  },
-  {
-    msg: `Hello guys! My name is Chris and I'm a french software engineer.`,
-    date: new Date(),
-  },
-  {
-    msg: `Hello guys! My name is Chris and I'm a french software engineer.`,
-    date: new Date(),
-  },
-  {
-    msg: `Hello guys! My name is Chris and I'm a french software engineer.`,
-    date: new Date(),
-  },
-  {
-    msg: `Hello guys! My name is Chris and I'm a french software engineer.`,
-    date: new Date()
   }
 ];
 
-for (const m in messages) {
+for (let m = 0; m < messages.length;  m++) {
   messages[m].id = m+1;
-  messages[m].msg += ` -- ${m+1}`;
 }
 
 export default messages;
