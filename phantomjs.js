855b37ea07612edaf666c94e5cdc2ee253c59f38
Chris MITEL <mitel.chris@gmail.com>
2019-01-10 23:20:41 +0100

    Updates phantomjs script test

--- phantomjs.js
+++ phantomjs.js
@@ -3,13 +3,27 @@ system = require('system'),
 address, output, size;
 
 address = 'http://localhost:8080/'
+// address = 'http://localhost:3000/'
 output = 'screenshots';
+
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
 
 page.open(address, function(status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
         phantom.exit();
     } else {
+
     page.render(output + '/phantomjs.png');
     phantom.exit();
     }
